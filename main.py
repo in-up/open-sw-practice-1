08b06e2bdadfe136507eeb813d174ae965c1128f
in-up <inup@kyonggi.ac.kr>
2024-10-02 00:12:22 +0900

    feat: implementing mul and div functions

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
 while True:
     formula = input("두 정수와 연산자를 포함한 수식 입력, 또는 'q'로 프로그램 종료 (예: 3 + 4)\n-> ")
     
@@ -24,6 +30,10 @@ while True:
         result = add(a, b)
     elif op == '-':
         result = sub(a, b)
+    elif op == '*':
+        result = mul(a, b)
+    elif op == '/':
+        result = div(a, b)
     else:
         print("[X] 유효하지 않은 연산자입니다.")
         continue
